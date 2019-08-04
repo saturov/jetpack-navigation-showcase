0c238724b84b1dc74721dcb7750f451d87bf8b31
Eugene Saturov <saturov@surfstudio.ru>
2019-08-04 14:09:58 +0300

    Инициализация Navigation Graph. Переход к первому фрагменту при старте приложения.

---
+++ app/src/main/java/ru/rebrain/navigation/HomeFragment.kt
@@ -0,0 +1,17 @@
+package ru.rebrain.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+}
--- app/src/main/java/ru/rebrain/navigation/MainActivity.kt
+++ app/src/main/java/ru/rebrain/navigation/MainActivity.kt
@@ -1,12 +1,19 @@
 package ru.rebrain.navigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
