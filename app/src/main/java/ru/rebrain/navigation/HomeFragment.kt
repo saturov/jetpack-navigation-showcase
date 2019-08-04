f414e702d98d79da9963050c23ebefc210f0bd2d
Eugene Saturov <saturov@surfstudio.ru>
2019-08-04 17:06:28 +0300

    Анимирование переходов с первого на второй фрагмент.

--- app/src/main/java/ru/rebrain/navigation/HomeFragment.kt
+++ app/src/main/java/ru/rebrain/navigation/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : Fragment() {
@@ -19,8 +20,16 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
         next_frag_btn.setOnClickListener {
-            findNavController().navigate(R.id.second_step_fragment_destination, null)
+            findNavController().navigate(R.id.second_step_fragment_destination, null, options)
         }
         next_activity_btn.setOnClickListener {
             findNavController().navigate(R.id.second_step_activity_destination, null)
