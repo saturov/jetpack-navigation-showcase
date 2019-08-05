bd9e4c3e2f3b46fd247da9d44406b1ce59730f29
Eugene Saturov <saturov@surfstudio.ru>
2019-08-05 20:24:06 +0300

    Навигация к фрагменту из Overflow Menu.

--- app/src/main/java/ru/rebrain/navigation/MainActivity.kt
+++ app/src/main/java/ru/rebrain/navigation/MainActivity.kt
@@ -1,8 +1,12 @@
 package ru.rebrain.navigation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initToolbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 
     private fun initToolbar() {
